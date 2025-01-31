dbcd2ea6fccc215e0374399a51ab0bc89e8da07f
EmaniAditya <esaadityareddy@gmail.com>
2025-01-31 23:47:04 +0530

    navbar border

--- blue-star-frontend/src/components/Navbar.tsx
+++ blue-star-frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <div className="flex justify-between p-4 border-b h-16">
+        <div className="flex justify-between p-4 h-16 rounded-xl border-2">
             <div className="flex items-center">
                 <Link href="/" className="text-xl font-bold">
                     Blue Star AZ
