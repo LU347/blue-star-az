f47b2f0845604bbad7443ca6d4f1ff9c735e43a8
EmaniAditya <esaadityareddy@gmail.com>
2025-01-31 23:31:04 +0530

    Added Navbar

---
+++ blue-star-frontend/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export default function Navbar() {
+    return (
+        <div className="flex justify-between p-4 border-b h-16">
+            <div className="flex items-center">
+                <Link href="/" className="text-xl font-bold">
+                    Blue Star AZ
+                </Link>
+            </div>
+            <div className="flex items-center space-x-4">
+                <Link href="login" className="px-4 py-2 rounded">
+                    Login
+                </Link>
+                <Link href="/signup" className="px-4 py-2 bg-blue-500 rounded">
+                    Signup
+                </Link>
+            </div>
+        </div>
+    )
+}
