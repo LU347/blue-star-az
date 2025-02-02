f258ab4d20021004c8e6f20ab2a17ed28e2ee7a6
Lucile O. <103535661+LU347@users.noreply.github.com>
2025-02-01 18:02:52 -0600

    Revert "Feature/login create page (#7)"
    This reverts commit 529ea848a7abe87aa9201619be5ade1c8288c3d1.

--- blue-star-frontend/src/app/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-
-const Profile: React.FC = () => {
-    return (
-        <h1 className="flex justify-center items-center h-screen">User Profile</h1>
-    );
-}
-
-export default Profile;
--- blue-star-frontend/src/app/signup/page.tsx
+++ blue-star-frontend/src/app/signup/page.tsx
@@ -1,64 +1,16 @@
-import FormComponent from "@/components/FormComponent/Form";
+import Link from 'next/link';
 
-const Signup: React.FC = () => {
-    const signupFields = [
-        {
-            name: "email",
-            label: "Email Address",
-            type: "email",
-            placeholder: "Email Address",
-            ariaLabel: "email_address_label"
-        },
-        {
-            name: "password",
-            label: "Password",
-            type: "password",
-            placeholder: "Password",
-            ariaLabel: "password_label"
-        },
-        {
-            name: "confirmPassword",
-            label: "Confirm Password",
-            type: "password",
-            placeholder: "Password",
-            ariaLabel: "confirm_password_label"
-        },
-        {
-            name: "phoneNumber",
-            label: "Phone Number",
-            type: "tel",
-            placeholder: "Phone Number",
-            ariaLabel: "phone_number_label"
-        },
-        {
-            name: "userType",
-            label: "Are you a Service Member or Volunteer?",
-            type: "select",
-            placeholder: "Select Status",
-            ariaLabel: "user_type_label",
-            options: [
-                { value: "service_member", label: "Service Member" },
-                { value: "volunteer", label: "Volunteer" }
-            ]
-        }
-    ];
-
+export default function Signup() {
     return (
-        //TODO: If the user is a service member, it should redirect them to signup/userInfo after creating their account
-        
-        <div className="flex flex-col items-center justify-center h-screen">
-            <FormComponent
-                title="Create an Account"
-                action="/signup"
-                formName="signupForm"
-                fields={signupFields}
-                buttonText="Sign Up"
-                linkText="Already have an account? Login"
-                linkHref="/login"
-                ariaLabel="Sign Up Form"
-            />
+        <div className="signupPage">
+            <form name="signupForm">
+                <label htmlFor="username">Username</label>
+                <input type="text" placeholder="Username"></input>
+                <label htmlFor="password">Password</label>
+                <input type="text" placeholder="Password"></input>
+                <button type="submit">Login</button>
+            </form>
+            <Link href="login">Log in</Link>
         </div>
     );
-};
-
-export default Signup;
+}
