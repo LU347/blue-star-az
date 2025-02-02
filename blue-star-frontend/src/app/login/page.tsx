529ea848a7abe87aa9201619be5ade1c8288c3d1
Lucile O. <103535661+LU347@users.noreply.github.com>
2025-02-01 18:00:16 -0600

    Feature/login create page (#7)
    * Create reusable form component
    
    * Updated login / create page forms
    
    * Light/Dark mode styling
    
    * Update input text-color + form width + form margins
    
    * Aria Labels
    
    * Submit user info form
    
    * Updated border colors
    
    * Update address fields + css

--- blue-star-frontend/src/app/login/page.tsx
+++ blue-star-frontend/src/app/login/page.tsx
@@ -1,16 +1,37 @@
-import Link from 'next/link';
+import FormComponent from "@/components/FormComponent/Form";
 
-export default function Login() {
+const Login: React.FC = () => {
+    const loginFields = [
+        {
+            name: "email",
+            label: "Email Address",
+            type: "email",
+            placeholder: "Email Address",
+            ariaLabel: "email_address_label",
+        },
+        {
+            name: "password",
+            label: "Password",
+            type: "password",
+            placeholder: "Password",
+            ariaLabel: "password_label",
+        },
+    ];
+
     return (
-        <div className="loginPage">
-            <form name="loginForm">
-                <label htmlFor="username">Username</label>
-                <input type="text" placeholder="Username"></input>
-                <label htmlFor="password">Password</label>
-                <input type="text" placeholder="Password"></input>
-                <button type="submit">Login</button>
-            </form>
-            <Link href="signup">Create Account</Link>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <FormComponent
+                title="Log in"
+                action="/login"
+                formName="loginForm"
+                fields={loginFields}
+                buttonText="Login"
+                linkText="Create Account"
+                linkHref="/signup"
+                ariaLabel="Log in Form"
+            />
         </div>
     );
-}
+};
+
+export default Login;
--- blue-star-frontend/src/components/Navbar.tsx
+++ blue-star-frontend/src/components/Navbar.tsx
@@ -2,18 +2,21 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <div className="flex justify-between p-4 h-16 rounded-xl border-2">
+        <div className="flex justify-between p-4 h-16 rounded-xl border-b-2 border-slate-500">
             <div className="flex items-center">
                 <Link href="/" className="text-xl font-bold">
                     Blue Star AZ
                 </Link>
             </div>
             <div className="flex items-center space-x-4">
-                <Link href="login" className="px-4 py-2 rounded">
+                <Link href="/login" className="px-4 py-2 rounded">
                     Login
                 </Link>
-                <Link href="/signup" className="px-4 py-2 bg-blue-500 rounded">
+                <Link href="/signup" className="px-4 py-2 rounded">
                     Signup
+                </Link>
+                <Link href="/profile" className="px-4 py-2 rounded">
+                    Profile
                 </Link>
             </div>
         </div>
