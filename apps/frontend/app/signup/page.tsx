652f9aba93703f3cdd26cd24aed6f11155042a4c
LU347 <103535661+LU347@users.noreply.github.com>
2025-02-05 00:58:08 -0600

    Update form fields
    Removed the option to register an account as a volunteer and added the required fields for service members.

--- apps/frontend/app/signup/page.tsx
+++ apps/frontend/app/signup/page.tsx
@@ -1,5 +1,14 @@
 import FormComponent from "@/components/FormComponent/Form";
 
+/*
+    Required fields:
+    email, password, first & last name, gender, branch
+
+    Optional:
+    phoneNumber, address fields (since there's different types of military addresses)
+    Address format based on https://support.govx.com/article?id=1176&sectionId=1025
+*/
+
 const Signup: React.FC = () => {
     const signupFields = [
         {
@@ -7,46 +16,124 @@ const Signup: React.FC = () => {
             label: "Email Address",
             type: "email",
             placeholder: "Email Address",
-            ariaLabel: "email_address_label"
+            ariaLabel: "email_address_label",
+            required: true
         },
         {
             name: "password",
             label: "Password",
             type: "password",
             placeholder: "Password",
-            ariaLabel: "password_label"
+            ariaLabel: "password_label",
+            required: true
         },
         {
             name: "confirmPassword",
             label: "Confirm Password",
             type: "password",
             placeholder: "Password",
-            ariaLabel: "confirm_password_label"
+            ariaLabel: "confirm_password_label",
+            required: true
         },
         {
             name: "phoneNumber",
             label: "Phone Number",
             type: "tel",
             placeholder: "Phone Number",
-            ariaLabel: "phone_number_label"
+            ariaLabel: "phone_number_label",
+            required: true,
+
         },
         {
-            name: "userType",
-            label: "Are you a Service Member or Volunteer?",
+            name: "first_name",
+            label: "First Name",
+            type: "text",
+            placeholder: "First Name",
+            ariaLabel: "first_name_label",
+            required: true,
+        }, 
+        {
+            name: "last_name",
+            label: "Last Name",
+            type: "text",
+            placeholder: "Last Name",
+            ariaLabel: "last_name_label",
+            required: true,
+        },
+        {
+            name: "gender",
+            label: "Gender",
             type: "select",
-            placeholder: "Select Status",
-            ariaLabel: "user_type_label",
+            placeholder: "Select Gender",
+            ariaLabel: "gender_label",
             options: [
-                { value: "service_member", label: "Service Member" },
-                { value: "volunteer", label: "Volunteer" }
+                { value: "male", label: "Male" },
+                { value: "female", label: "Female" },
+            ],
+            required: true,
+        },
+        {
+            name: "military_branch",
+            label: "Branch",
+            type: "select",
+            placeholder: "Select Military Branch",
+            ariaLabel: "military_branch_label",
+            options: [
+                { value: "air_force", label: "Air Force" },
+                { value: "army", label: "Army" },
+                { value: "coast_guard", label: "Coast Guard"},
+                { value: "marines", label: "Marines" },
+                { value: "navy", label: "Navy" },
+                { value: "space_force", label: "Space Force" }
+            ],
+            required: true,
+        },
+        {
+            name: "address_one",
+            label: "Address Line 1",
+            type: "text",
+            placeholder: "USS Nimitz (CVN 68) / PSC 1234 / Unit 7400",
+            ariaLabel: "address_one_label",
+        },
+        {
+            name: "address_two",
+            label: "Address Line 2",
+            type: "text",
+            placeholder: "DPO, AE, 09498-0048 / APO AE 09204-1234",
+            ariaLabel: "address_two_label"
+        },
+        {
+            name: "city",
+            label: "City / Town",
+            type: "text",
+            placeholder: "City / Town",
+            ariaLabel: "city_label",
+        },
+        {
+            name: "state",
+            label: "State / Province",
+            type: "text",
+            placeholder: "State / Province",
+            ariaLabel: "state_label",
+        },
+        {
+            name: "country",
+            label: "Country",
+            type: "select",
+            placeholder: "Country",
+            ariaLabel: "country_label",
+            options: [
+                { value: "usa", label: "United States of America" },
+                { value: "germany", label: "Germany" },
+                { value: "bahrain", label: "Bahrain" },
+                { value: "japan", label: "Japan" },
+                { value: "philippines", label: "Philippines"}
             ]
         }
     ];
 
     return (
-        //TODO: If the user is a service member, it should redirect them to signup/userInfo after creating their account
-        
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-auto overflow-scroll m-8">
             <FormComponent
                 title="Create an Account"
                 action="/signup"
