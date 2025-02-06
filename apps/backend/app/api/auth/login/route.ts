1fb3c65f1d35ba1bed5b97e9af117c2831154fd0
LU347 <103535661+LU347@users.noreply.github.com>
2025-02-05 22:07:26 -0600

    Generate session tokens on login

--- apps/backend/app/api/auth/login/route.ts
+++ apps/backend/app/api/auth/login/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { Error, Status } from "app/types/enums";
 
 const prisma = new PrismaClient();
@@ -35,7 +36,9 @@ export async function POST(req: Request) {
             return NextResponse.json({ error: Error.INVALID_CREDENTIALS }, { status: 401 });
         }
 
-        return NextResponse.json({ message: Status.LOGIN_SUCCESS, userType: user.userType }, { status: 200 });
+        const token = jwt.sign({ userId: user.id, userType: user.userType }, `${process.env.JWT_SECRET}`, { expiresIn: '1h' });
+
+        return NextResponse.json({ message: Status.LOGIN_SUCCESS, token }, { status: 200 });
     } catch (error) {
         console.error("Login error:", error);
         return NextResponse.json({ error: Error.INTERNAL_ERR }, { status: 500 });
